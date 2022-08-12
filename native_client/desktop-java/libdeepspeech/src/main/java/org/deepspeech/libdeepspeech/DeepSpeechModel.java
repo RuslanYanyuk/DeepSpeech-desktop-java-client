c360e6e6ce8ddaa5c91ee18e81c6a678c0f4268c
Ruslan Yaniuk <ruslan.yanyuk@gmail.com>
2022-08-12 18:16:36 +0300

    Add cmake

--- native_client/desktop-java/libdeepspeech/src/main/java/org/deepspeech/libdeepspeech/DeepSpeechModel.java
+++ native_client/desktop-java/libdeepspeech/src/main/java/org/deepspeech/libdeepspeech/DeepSpeechModel.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import sun.security.action.GetPropertyAction;
 
 /**
  * @brief Exposes a DeepSpeech model in Java
